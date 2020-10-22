9fd136e2fbbd3bf9937a134f68c25e3e2ac5e203
LulasValadez99 <72040783+LulasValadez99@users.noreply.github.com>
2020-10-22 14:05:51 -0500

    Update scanf.c

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-#include <math.h>
-#define PI 3.14159
-int main()
+# include <stdio.h>
+# include <math.h>
+# define  PI  3.14159
+int  main ()
 {
 int r;
-float l,a,b,c;//Declaro variables
-printf("Introduce radio (entero): ");
-scanf("%d", &r);
+float l, a;
+printf("Introducir radio (entero): ");
+scanf("% d", &r);
+l = 2*PI*r;
+printf(" La longitud de la circunferencia vale %0.3f \n", l);
 
-printf("Dame el \'primer\' numero: ");
-scanf("%d",&b);
-
-printf("Dame el \'segundo\' numero: ");
-scanf("%d",&c);
-
-printf("Multiplicacion\n");
-l=2*PI*r;
-printf("\t%0.3f * %0.3f * %0.3f = %0.3f\n");
-
-printf("\nPotencia?\n");
-a=pow (PI*r,b);
-printf("\t%0.3f * %0.3f elevado a la potencia / %0.3f es %0.3f\n", a, b);
+printf("\Potencia\n ", l);
+a = pow(PI*r,r);
+printf(" El área del círculo vale %0.3f \n ", a);
 }
