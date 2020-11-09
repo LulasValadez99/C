29e4007e9c5fd31c7507b1107c527eec4e031ecf
LulasValadez99 <72040783+LulasValadez99@users.noreply.github.com>
2020-11-08 23:40:23 -0600

    Update switchcase.c

--- control/switchcase.c
+++ control/switchcase.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>  
 int main()
 {
 	int opcion;
-	printf("Elige una opciÃ³n:");
+	float a= 10;
+	float b= 20;
+	float c;
+	printf("Elige una opción\n:");
 	printf("\t 1.- Suma:\n");
 	printf("\t 2.- Resta:\n");
-	printf("\t 6.- Salir:\n");
+	printf("\t 3.- Multiplicación:\n");
+	printf("\t 4.- División:\n");
+	printf("\t 5.- Potencia:\n");
+	printf("\t 6.- Raíz Cuadrada:\n");
+	printf("\t 7.- Salir:\n");
 	scanf("%d",&opcion);
 	switch(opcion)
 	{
 	case 1: 
-		c= a +b ;
+		c= a + b;
+		printf("La suma es: %f", c);
 		break;
 	case 2: 
+	    c= a - b;
+		printf("La suma es: %f0.3", c);
+		break;
+	case 3: 
+	    c= a * b;
+		printf("La suma es: %f0.3", c);
+		break;
+	case 4: 
+	    c= a / b;
+		printf("La suma es: %f0.3", c);
+		break;
+	case 5: 
+	    c= pow (a,b);
+		printf("La suma es: %f0.3", c);
+		break;
+	case 6: 
+	    c= sqrt (a/b);
+		printf("La suma es: %f0.3", c);
 		break;
 	default:
 		printf("No estÃ¡ puesta correctamente la unidad\n");
 		exit(0);
 	}
-	printf("\nLas %f pulgadas son %f centÃ­metros\n", pul, cm);
+	printf("\nFin de Switch\n");
 } 
